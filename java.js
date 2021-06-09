d1688989bc49ba9e77aea66e1910f6962df4753e
Otabek <otabekabdulazizov282@gmail.com>
2021-06-09 21:33:48 +0500

    dfsa a

---
+++ java.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.tail__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
+scjnscjns
